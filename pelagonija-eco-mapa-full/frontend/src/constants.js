f476763fc6fd6353121e62ebd7de393aa4bc528a
pavelgagachev1-svg <pavelgagachev1@gmail.com>
2026-09-22 23:01:56 +0200

    Update constants.js

--- pelagonija-eco-mapa-full/frontend/src/constants.js
+++ pelagonija-eco-mapa-full/frontend/src/constants.js
@@ -3,3 +3,4 @@ export const MAP_EDIT_URL =
 export const MAP_EMBED_URL =
   "https://www.google.com/maps/d/embed?mid=1UU0Ar9nAG9v0mUkHMQayg_M8olNHs5c";
 export const CONTACT_EMAIL = "ecterbt@gmail.com";
+export const GMAIL_COMPOSE_URL = `https://mail.google.com/mail/?view=cm&fs=1&to=${CONTACT_EMAIL}`;
