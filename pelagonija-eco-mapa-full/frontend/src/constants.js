5199860f314794bb12aa90ea64c49e5db35e6eda
pavelgagachev1-svg <pavelgagachev1@gmail.com>
2026-09-22 23:23:22 +0200

    Update constants.js

--- pelagonija-eco-mapa-full/frontend/src/constants.js
+++ pelagonija-eco-mapa-full/frontend/src/constants.js
@@ -4,3 +4,4 @@ export const MAP_EMBED_URL =
   "https://www.google.com/maps/d/embed?mid=1UU0Ar9nAG9v0mUkHMQayg_M8olNHs5c";
 export const CONTACT_EMAIL = "ecterbt@gmail.com";
 export const GMAIL_COMPOSE_URL = `https://mail.google.com/mail/?view=cm&fs=1&to=${CONTACT_EMAIL}`;
+export const ECTER_WEBSITE_URL = "https://www.ecter.org/";
